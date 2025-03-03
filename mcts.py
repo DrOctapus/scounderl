61fd961fee602b8a35f7feb6d92f74af74d26a42
DrOctapus <andrpanago333@gmail.com>
2025-03-03 02:26:23 +0200

    mcts demo

--- mcts.py
+++ mcts.py
@@ -1,20 +1,23 @@
 from __future__ import annotations
 import math
+import random
 
 USCB1CONST = 2
+random.seed("test")
 
 
 class Node:
-    def __init__(self, state: State, move: Move = None, t=0, n=0):
+    def __init__(self, state: State, move: Move = None, parent: Node = None, t=0, n=0):
         self.t = t
         self.n = n
         self.children: list[Node] = []
         self.state = state
         self.move = move
+        self.parent = parent
 
     def run(self):
         node = self.getBestChild()
-        if node.n == 0:
+        if node.n <= 10:
             node.rollover()
         else:
             node.expand().rollover()
@@ -27,9 +30,8 @@ class Node:
         max_child = None
         for child in self.children:
             if child.n == 0:
-                max_child = child
-                break
-            
+                return child
+
             score = child.t / child.n + USCB1CONST * math.sqrt(math.log(self.n) / child.n)
             if max_score == None or score > max_score:
                 max_score = score
@@ -37,31 +39,50 @@ class Node:
         return max_child.getBestChild()
 
     def expand(self):
-        for state, move in self.state.getMoves():
-            self.children.append(Node(state, move))
-        return self.children[0]
+        if len(self.children) == 0:
+            for state, move in self.state.getMoves():
+                self.children.append(Node(state, move, self))
+
+        return random.choice(self.children)
 
     def rollover(self):
-        pass
+        current_state = self.state.copy()
+        while True:
+            if current_state.isTerminal():  # TODO: make early stopping
+                return self.backpropogate(current_state.getScore())
+            current_state, _ = random.choice(current_state.getMoves())  # TODO: make not random
+
+    def backpropogate(self, t):
+        self.t += t
+        self.n += 1
+        if self.parent:
+            self.parent.backpropogate(t)
+        return t
 
     def getBestMove(self) -> tuple[State, Move]:
         max_score = None
         max_child = None
         for child in self.children:
-            if child.t == 0:
+            if child.n == 0:
                 continue
-            score = child.n / child.t
+            score = child.t / child.n
             if max_score == None or score > max_score:
                 max_score = score
                 max_child = child
         return max_child.state, max_child.move
 
 
-class State:
+class State:    
     def getMoves() -> list[tuple[State, Move]]:
         pass
-    
+
     def getScore() -> int:
+        pass
+
+    def copy() -> State:
+        pass
+
+    def isTerminal() -> bool:
         pass
 
 
